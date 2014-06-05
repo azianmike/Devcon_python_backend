39af6915ae41d4628946daec4fc457539f7e14f3
michaelluo <michaeluo@gmail.com>
2014-06-05 16:35:11 -0500

    DO NOT USE ANY CODE VERBATIM. Vulnerable to SQL injection attacks

--- thumbsUp.py
+++ thumbsUp.py
@@ -2,6 +2,7 @@
 import MySQLdb
 import json
 
+#do not use code, sql injection attacks
 def thumbsUp(data, conn, cursor):
 	cursor.execute('update Location set thumbsUp = thumbsUp + 1 where location_id='
 	+ str(data['location_id']))
